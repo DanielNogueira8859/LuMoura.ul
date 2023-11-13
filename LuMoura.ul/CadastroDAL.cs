f65c2cb853bd3530d98cff36ec29005f6be3ef39
Alexsaglia <132571874+Alexsaglia@users.noreply.github.com>
2023-11-13 08:27:39 -0300

    update

--- LuMoura.ul/CadastroDAL.cs
+++ LuMoura.ul/CadastroDAL.cs
@@ -14,11 +14,11 @@ namespace LuMoura.ul
        
             public int FichaCadastro(string Nome, string CPF, string Telefone, string Email)
             {
-                //SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Lu_Moura; Integrated Security = True; Connect Timeout = 30; Encrypt = False");
-                //conn.Open();
-
-                SqlConnection conn = new SqlConnection(@"Data Source=FAC0539709W10-1;Initial Catalog=LuMoura.DB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False");
+                SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Lu_Moura; Integrated Security = True; Connect Timeout = 30; Encrypt = False");
                 conn.Open();
+
+                //SqlConnection conn = new SqlConnection(@"Data Source=FAC0539709W10-1;Initial Catalog=LuMoura.DB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False");
+                //conn.Open();
 
 
                 SqlCommand cmd = new SqlCommand("insert into Cliente output inserted.idCliente values('" + Nome + " ','" + CPF + "',' " + Telefone + " ',' " + Email + " ', getdate())", conn);
