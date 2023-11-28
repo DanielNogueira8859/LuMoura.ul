26615bd7f6a037d776aa51e1025f44010b148fc0
GustavoWWXZ <149507314+GustavoWWXZ@users.noreply.github.com>
2023-11-28 11:42:56 -0300

    update

--- LuMoura.ul/obj/Debug/net6.0-windows/LuMoura.ul.AssemblyInfo.cs
+++ LuMoura.ul/obj/Debug/net6.0-windows/LuMoura.ul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LuMoura.ul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d65f0acf9fd14ddfa261a8e65c46a8889c2a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LuMoura.ul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LuMoura.ul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SQLQuery17.sql
@@ -0,0 +1,2 @@
+﻿select * from Agendamentos
+select * from Cliente
